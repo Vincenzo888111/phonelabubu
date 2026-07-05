f1bccf951bc7f89a8b63fc07cf5aee87246b89a9
Vincenzo <kornvathanak@email.com>
2026-07-05 14:38:06 +0700

    Actually fix base path this time

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/phonelabubu/"
+  base: process.env.VERCEL ? "/" : "/phonelabubu/",
 });
